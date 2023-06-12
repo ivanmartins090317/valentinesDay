1d224844a96993619d3efd7a53b0122af96e0dad
Ivan Martins <ivanmartins.ilha@gmail.com>
2023-06-12 19:13:41 -0300

    beginning and end

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# Título do Projeto
+
+Projeto criado para testar frame tsParticles e deixar alguém feliz.
+
+## Referência
+
+ - [Google fonts]https://fonts.google.com/icons?icon.query=password
+ - [Icones]https://fontawesome.com/search?s=solid%2Cbrands
+ - [Code Pen]https://codepen.io/ivanmartins090317/pen/dyQYYeB
+ 
+
+## 🔗 Links
+
+https://particles.js.org/
+
---
+++ script.js
@@ -0,0 +1,29 @@
+function love() {
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+  };
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
+}
